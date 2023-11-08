194575fcc09c1d1ea5217993a892b9d51befc1e5
Remaking <142949432+Hadaward30@users.noreply.github.com>
2023-11-08 14:47:57 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,33 +2,33 @@ import streamlit as st
 
 def calculadora():
 
-    st.title('CALCULADORa')
+    st.title('Calculadora')
     operacao = st.selectbox("Selecione a operação", ("Soma", "Subtração", "Multiplicação", "Divisão", "Raiz quadrada"))
 
     if operacao == 'Raiz quadrada':
         num1 = st.number_input('Informe o número que deseja descobrir a raiz quadrada', 0)
-        if st.button("Converter"):
+        if st.button("CALCULAR"):
             st.success('A raiz quadrada de {} é {:.2f}'.format(num1, num1 ** (1 / 2)))
 
     elif operacao == 'Soma':
         num1 = st.number_input("Insira o primeiro número")
         num2 = st.number_input("Insira o segundo número")
         resultado = num1 + num2
-        if st.button("Converter"):
+        if st.button("CALCULAR"):
             st.success('{:.2f} + {:.2f} = {:.2f}'.format(num1, num2, resultado))
 
     elif operacao == 'Subtração':
         num1 = st.number_input("Insira o primeiro número")
         num2 = st.number_input("Insira o segundo número")
         resultado = num1 - num2
-        if st.button("Converter"):
+        if st.button("CALCULAR"):
             st.success('{:.2f} - {:.2f} = {:.2f}'.format(num1, num2, resultado))
 
     elif operacao == 'Multiplicação':
         num1 = st.number_input("Insira o primeiro número")
         num2 = st.number_input("Insira o segundo número")
         resultado = num1 * num2
-        if st.button("Converter"):
+        if st.button("CALCULAR"):
             st.success('{:.2f} * {:.2f} = {:.2f}'.format(num1, num2, resultado))
 
     elif operacao == 'Divisão':
@@ -39,6 +39,6 @@ def calculadora():
         else:
             st.error("Divisão por zero não é permitida")
 
-        if st.button("Converter"):
+        if st.button("CALCULAR"):
             st.success('{:.2f} ÷ {:.2f} = {:.2f}'.format(num1, num2, resultado))
 
